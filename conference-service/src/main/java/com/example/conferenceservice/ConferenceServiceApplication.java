363e153886ac4ace2357434a3fbfe0b6571061fc
jgkz123 <zldtmshdn@gmail.com>
2026-09-01 16:34:16 +0900

    feat/yaml 파일 설정

--- conference-service/src/main/java/com/example/conferenceservice/ConferenceServiceApplication.java
+++ conference-service/src/main/java/com/example/conferenceservice/ConferenceServiceApplication.java
@@ -1,4 +1,4 @@
-package org.example.conferenceservice;
+package com.example.conferenceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
